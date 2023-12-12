9a565a42b2fe9974112c1885ec241726a38a3a02
Vermaas <vermaas@astron.nl>
2023-12-12 16:49:02 +0100

    testing out production database

--- mom3api/settings/dev.py
+++ mom3api/settings/dev.py
@@ -1,1 +1,12 @@
 from mom3api.settings.base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lofar_mom3',
+        'USER': 'root',
+        'PASSWORD': 'my_sql_2023_!@#',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
+}
